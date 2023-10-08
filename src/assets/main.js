86a13045ef79d5901a5a8701137209b80d62e614
Mariano Fuentes <hardhopeid@gmail.com>
2023-10-08 16:19:43 -0500

    Consumiendo API

---
+++ src/assets/main.js
@@ -0,0 +1,50 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCqNVdGTtmjXxJfFSjTgbnQQ&part=snippet%2Cid&order=date&maxResults=50';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '059d9a1e9dmshc03085e1b016835p1e522bjsn06d77a870412',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+/* try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+} */
+
+async function FetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await FetchData(API);
+        let view =  `
+        ${videos.items.map(video => ` 
+            <div class="group relative">
+                <div
+                    class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                    <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                    <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                    </h3>
+                </div>
+            </div>
+        `).slice(0,4).join('')}
+            
+        `;
+    } catch (error) {
+        console.error(error);
+    }
+})();
